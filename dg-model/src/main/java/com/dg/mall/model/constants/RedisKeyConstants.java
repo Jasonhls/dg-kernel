c9bb9b0ec40498888b3eae3dee37b3b577e08b55
王强 <wangqiang@dg-mall.com>
2019-08-02 15:03:15 +0800

    no message

---
+++ dg-model/src/main/java/com/dg/mall/model/constants/RedisKeyConstants.java
@@ -0,0 +1,14 @@
+package com.dg.mall.model.constants;
+
+/**
+ * @description: Redis key 常量
+ * @author: wangqiang
+ * @create: 2019-08-02 10:39
+ **/
+public interface RedisKeyConstants {
+
+    /**
+     * 系统管理用户token
+     */
+    String SYSTEM_USER_TOKEN_KEY = "system_user_token:";
+}
